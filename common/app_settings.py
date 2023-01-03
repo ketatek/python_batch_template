9f10f75ecf7ba88ce902e255056cc929ed4c20fe
keita.tokunaga <ketaketa1985@gmail.com>
2023-01-03 22:18:43 +0900

    アプリケーション設定ファイルをtomlを前提にするよう変更 アプリケーション設定の取り扱いモジュールにについて、ちょっとしたコードを追記 クラス設計のissueをコメントに追記。

--- common/app_settings.py
+++ common/app_settings.py
@@ -1,4 +1,43 @@
 #-----------------------------------------
 # バッチアプリの設定 の アクセッサなどを実装
-# configParserを前提とするため、フォーマットはjsonかiniを前提とする。
+# Python 3.11 でtoml パーサーが提供されるようになったため、
+# tomlを前提とすることにした。
 #-----------------------------------------
+import tomllib
+from pprint import pprint
+
+
+
+class AppSettings():
+"""
+# 設定ファイル(toml)の取扱クラス
+
+issue:  
+* portableな構成を前提に、以下のパスがデフォルトとしたい。  
+    <メインモジュール配置先>/config/settings.toml
+* ファイルの有無による動作を定義できるようにしたい。
+    * ファイルなしを許容するなら、デフォルト値を定義
+    * ファイルなしを許容しないなら、その場で例外スロー
+* globalな設定を前提とするため、singltonな形式で提供したい。
+* validaterの実装が必要。
+"""
+
+
+
+    
+
+    def get_settings_path(path: str) -> str:
+        
+        ret = ""
+
+        if path is not None:
+            ret = path
+
+        return ret
+
+    def load_settings(path: str):
+        
+        path = get_settings_path(path)
+
+        with open(path, mode="rb") as f:
+            tomllib.load(f)
