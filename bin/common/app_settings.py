60c51cde98976b0fae559af7fa49d08ab46b5ae4
keita.tokunaga <ketaketa1985@gmail.com>
2023-01-05 22:35:40 +0900

    設定ファイルパスの取得設定周りを修正

--- bin/common/app_settings.py
+++ bin/common/app_settings.py
@@ -4,6 +4,7 @@
 # tomlを前提とすることにした。
 #-----------------------------------------
 import tomllib
+import os 
 from pprint import pprint
 import utils
 
@@ -20,20 +21,96 @@ issue:
 * globalな設定を前提とするため、singltonな形式で提供したい。
 * validaterの実装が必要。
 """
-    
 
-    def get_settings_path(path: str) -> str:
-        
-        ret = str(utils.get_root_path() / 'config' / 'settings.toml')
+    def __init__(self, settings_path:str=None):
+        """
 
-        if path is not None:
-            ret = path
+        Args:
+            settings_path (str, optional): 設定ファイルパス。 Defaults to None.
+        """
+
+        # 設定ファイルのパスを指定
+        self.settings_path = settings_path
+
+        # 設定ファイルを読込
+        self._load_settings()        
+
+    @property
+    def settings_raw(self) -> dict:
+        """
+        Returns:
+            dict: 設定ファイル読込データの生データ
+        """
+        return self._settings
+
+    @property
+    def settings_path(self) -> str:
+        """設定ファイルのパスを取得
+            self.__settings_path is None ならデフォルトのパスを返す。
+
+            default path:
+                    <app_settings.py(this scripts) in dir>/../../config/settings.toml
+        Returns:
+            str: 設定ファイルのパス
+        """
+
+        # 設定ファイルのパスを設定
+        ret = self.__settings_path
+
+        # 設定ファイルのパス指定がなければ、デフォルトのパスを返す。
+        if ret is None:
+            ret = str(utils.get_root_path() / 'config' / 'settings.toml')
 
         return ret
 
-    def load_settings(path: str):
-        
-        path = get_settings_path(path)
+    @property
+    def settings_path(self) -> str:
+        """設定ファイルのパスを取得  
+            self.__settings_path is None ならデフォルトのパスを返す。
 
+            * default path:  
+                * <app_settings.py(this scripts) in dir>/../../config/settings.toml
+        Returns:
+            str: 設定ファイルのパス
+        """
+        
+        # 設定ファイルのパスを設定
+        ret = self.__settings_path
+
+        # 設定ファイルのパス指定がなければ、デフォルトのパスを返す。
+        if ret is None:
+            ret = str(utils.get_root_path() / 'config' / 'settings.toml')
+
+        return ret
+
+    @property.setter
+    def settings_path(self, value: str) -> str:
+        """
+        Args:
+            value (str): 設定ファイルのパスを指定
+
+        Returns:
+            str: 設定ファイルのパス
+        """
+
+        if not os.path.exists(value):
+            raise Error(f"指定されたパスは存在しません。> {value}")
+
+        # 設定ファイルのパスを設定
+        self.__settings_path = value
+
+        return self.__settings_path
+
+    def _load_settings(self) -> dict:
+        """self.settings_path のパスに配置されている、
+        設定ファイルをを読込んで、self.__settingsに設定
+
+        Returns:
+            dict: 設定ファイルデータ
+        """
+
+        path = self.settings_path
         with open(path, mode="rb") as f:
-            tomllib.load(f)
+            self.settings = tomllib.load(f)
+
+        return self.settings
